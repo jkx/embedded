c026d7926ca9e1a2c63509b85359e29e04960cb0
Jkx <jkx@larsen-b.com>
2016-01-20 21:57:12 +0000

    Changed from to hostname

--- opt/bin/test_email.py
+++ opt/bin/test_email.py
@@ -2,9 +2,12 @@
 
 from email.mime.text import MIMEText
 from subprocess import Popen, PIPE
+import commands 
 
+
+from_ = commands.getoutput('hostname')
 msg = MIMEText("\nCeci est un texte d'exemple donc ..\nComment tu vas ?\nA++")
-msg["From"] = "bananapi-1@larsen-b.com"
+msg["From"] = from_+"@larsen-b.com"
 msg["To"] = "jkx@larsen-b.com"
 msg["Subject"] = "Salut"
 p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
