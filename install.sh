b66f78d8beeb13411eb9e2eadab1314e684f7624
Jkx <jkx@larsen-b.com>
2016-03-29 17:20:05 +0200

    Fix added remote syslog

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-sudo ln -s ./opt/bin /opt
+sudo ln -s ~/embedded/opt/bin /opt
+sudo ln -s ~/embedded/etc/default/busybox-syslogd /etc/default
+
 ln -sf ~/embedded/config/.vimrc ~/.vimrc
 ln -sf ~/embedded/config/.screenrc ~/.screenrc
 
